ae8d24a1605c7b1f6065f14c646697f3fb47d73c
Seth Brown <sethbrown19@outlook.com>
2022-06-01 22:33:12 -0400

    started first spring demo project.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Java-Spring
+
 Learing Java Spring 
---
+++ spring-demo-one/src/main/java/com/example/springdemoone/SpringDemoOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdemoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoOneApplication.class, args);
+    }
+
+}
